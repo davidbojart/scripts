1618a6278aa3e5111dc65aa957c04f20a87d7976
David B.R <eldavis890@openmailbox.org>
2013-12-02 09:15:46 +0100

    Update new_ip.sh
    - Se añade un nuevo if que verifica si ha podido obtener la nueva IP. Le llega un correo al usuario root.
    - Se añade un log donde se almacenan la diferentes direcciones IP que ha obtenido.

--- new_ip.sh
+++ new_ip.sh
@@ -8,9 +8,16 @@ RUTA=`TU RUTA DONDE SE ENCUENTRA EL SCRIPT`
 IPANTERIOR="0.0.0.0"
 MIP=`/usr/bin/curl icanhazip.com 2>/dev/null`
 
+
 if [ "$MIP" != "$IPANTERIOR" ] ; then
 
-	/bin/sed -i "s|$IPANTERIOR|$MIP|g" $RUTA/reporte.sh
-	/bin/echo -e "La IP ha cambiado en la Rapsberry.\n\nNueva IP: $MIP" | /usr/bin/mail -s "IP Nueva en Raspberry" YOUR@MAIL.COM
+	if [ "$MIP" == "" ] ; then
+	
+		echo "No se ha podido obtener la nueva IP"
 
+	else
+             	/bin/sed -i "s|$IPANTERIOR|$MIP|g" $RUTA/new_ip.sh
+             	/bin/echo -e "La IP ha cambiado en la Rapsberry.\n\nNueva IP: $MIP" | /usr/bin/mail -s "IP Nueva en Raspberry" tumail@mail.com
+             	/bin/echo "$MIP" >> $RUTA/.mip     
+        fi
 fi
